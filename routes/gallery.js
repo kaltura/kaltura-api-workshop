c1b9ae9f14e8a003f4be7ec541e26ce45bbd69a9
Omri Katz <omri.katz@kaltura.com>
2020-01-19 17:10:39 +0200

    feat: add gallery page

---
+++ routes/gallery.js
@@ -0,0 +1,45 @@
+var kaltura = require("kaltura-client");
+var express = require("express");
+var R = require("ramda");
+var router = express.Router();
+var KalturaClientFactory = require("../lib/kalturaClientFactory");
+
+function getPlaylists(client) {
+    return new Promise((resolve, reject) => {
+        /*
+          Task 6
+          ======
+          get all playlists
+
+          Information:
+          ------
+          available arguments:
+          for success: resolve response
+          for not success: reject response
+         */
+
+        let filter = new kaltura.objects.PlaylistFilter();
+        filter.orderBy = kaltura.enums.PlaylistOrderBy.CREATED_AT_ASC;
+        let pager = new kaltura.objects.FilterPager();
+        pager.pageSize = 500;
+        kaltura.services.playlist
+            .listAction(filter, pager)
+            .execute(client)
+            .then(response => {
+                resolve(response);
+            });
+    });
+}
+
+router.get("/", async (req, res, next) => {
+    var ks = await KalturaClientFactory.getKS("", { type: kaltura.enums.SessionType.ADMIN });
+    var client = await KalturaClientFactory.getClient(ks);
+    var playlists = await getPlaylists(client);
+    playlists = playlists.objects.filter(playlist => playlist.playlistContent);
+    playlists = R.uniqWith((a, b) => {
+        return a.userId === b.userId;
+    }, playlists);
+    res.render("gallery", { playlists });
+});
+
+module.exports = router;
